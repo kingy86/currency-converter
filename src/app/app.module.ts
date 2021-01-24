14cb293c04cc40b1213b5834b0a81b668ac4458f
kingy86 <nirvanafoo86@hotmail.com>
2021-01-24 11:29:45 +0000

    adding about and currency converter components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    CurrencyConverterComponent
   ],
   imports: [
     BrowserModule,
