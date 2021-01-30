590f713172d24e408934ef8aa34c934327536b0b
Mark <nirvanafoo86@hotmail.com>
2021-01-30 19:40:25 +0000

    fetching data from api

--- src/app/historic-data/historic-data.component.ts
+++ src/app/historic-data/historic-data.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ExchangeRateService } from '../../services/exchange-rate.service';
 
 @Component({
   selector: 'app-historic-data',
   templateUrl: './historic-data.component.html',
-  styleUrls: ['./historic-data.component.css']
+  styleUrls: ['./historic-data.component.css'],
 })
 export class HistoricDataComponent implements OnInit {
+  // historicRates!: {
 
-  constructor() { }
+  // }
 
-  ngOnInit(): void {
+  constructor(private service: ExchangeRateService) {
+    this.service
+      .getHistoricRates('ILS', 'JPY', '2021-01-30', '2020-10-01')
+      .subscribe((result) => {
+        console.log('Historic Data : ' + result);
+      });
   }
 
+  ngOnInit(): void {}
 }
