d19c4b27efcd5d1061a6c7956208efc27fd729e2
Mark <nirvanafoo86@hotmail.com>
2021-01-31 20:21:06 +0000

    seperating key value pairs of rates

--- src/app/historic-data/historic-data.component.ts
+++ src/app/historic-data/historic-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ExchangeRateService } from '../../services/exchange-rate.service';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-historic-data',
@@ -10,12 +11,23 @@ export class HistoricDataComponent implements OnInit {
   rates!: {};
   from = 'GBP';
   year = '2020';
+  data = [];
 
   loadHistoricRates() {
     this.service.getHistoricRates(this.from, this.year).subscribe((result) => {
       this.rates = result.rates;
-      console.log(this.rates);
     });
+  }
+
+  fetchDates() {
+    let dates = Object.keys(this.rates);
+    return dates;
+  }
+
+  fetchValues() {
+    let objOfRates = Object.values(this.rates);
+    let keyOfObj = Object.values(objOfRates);
+    return Object.values(keyOfObj);
   }
 
   constructor(private service: ExchangeRateService) {
